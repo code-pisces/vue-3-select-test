954555b590db1b1a0ed654249ce7f7fce4915ac8
Amorim <guh.contato.work@outlook.com.br>
2022-04-01 20:15:20 -0300

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,66 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="hello-world">
+    <div class="form-group">
+      <h2>Select com Pesquisa</h2>
+      <vue-search-select
+        :options="items"
+        v-model="selectedFromSelect"
+        @search="handleWithSearchFromApi"
+        :clearable="false"
+        :filterable="false"
+      >
+        <template #option="{ label, value }">
+          <p><b>ID: </b>{{ value }}</p>
+          <p>{{ label }}</p>
+        </template>
+
+        <template #open-indicator>
+          {{ '<' }}
+        </template>
+      </vue-search-select>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+<script setup>
+import { ref, watch } from 'vue'
+const items = ref([])
+const selectedFromSelect = ref('')
+
+watch(selectedFromSelect, (value) => {
+  console.log(value)
+})
+
+const options = [
+  'Azul',
+  'Amarelo',
+  'Marrom',
+]
+
+const handleWithSearchFromApi = (text, loading) => {
+  loading(true)
+  const filtrado = options.filter((value) => {
+    value.
+      toLocaleLowerCase()
+      .includes(text.toLocaleLowerCase())
+  })
+  console.log(filtrado)
+  items.value = filtrado
+  loading(false)
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style lang="scss" scoped>
+.hello-world {
+  display: grid;
+  height: 100vh;
+  align-content: center;
+  justify-content: center;
+
+  .form-group {
+    display: grid;
+    align-items: center;
+    justify-content: center;
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import VSelect from 'vue-select'
+import { loadFonts } from './plugins/webfontloader'
+import 'vue-select/dist/vue-select.css'
+loadFonts()
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(vuetify)
+  .component('vue-search-select', VSelect)
+  .mount('#app')
